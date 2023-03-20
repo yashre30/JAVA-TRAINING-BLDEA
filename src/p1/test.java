c71e3a3abdb5d2a44d0037b25ff1fdeb5fbecfea
yashre30 <kavisujatakavi@gmail.com>
2023-03-20 13:22:22 +0530

    first program

--- src/p1/test.java
+++ src/p1/test.java
@@ -4,6 +4,25 @@ public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int[] arr1= {1,2,4,5,6,7};
+		int[] arr2= {2,3,5,7};
+		int m=arr1.length;
+		int n=arr2.length; 
+		int i=0;
+		int j=0;
+		while(i<m && j<n) {
+			if(arr1[i] < arr2[j]) {
+				i++;
+			}
+			else if(arr1[i] >arr2[j]) {
+				j++;
+			}
+			else {
+				System.out.print(arr2[j]+" ");
+				j++;
+				i++;
+			}
+		}
 
 	}
 
